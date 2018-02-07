b8d107bbbd8490fd000092e096e1a7f881668ed2
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-07 20:08:04 +0100

    Finished instruction and game controls info

--- game/js/menu.js
+++ game/js/menu.js
@@ -6,6 +6,7 @@ const menuState = {
 
         const titleFontSize = currentGameHeight/26.85;
         const textFontSize = currentGameHeight/33.56;
+        const textLineSpacing = currentGameHeight/10;
 
         menuBackGround = game.add.tileSprite(0, 0, gameWidth, gameHeight, 'background');
         menuBackGround.tileScale = new Phaser.Point(backgroundWidthRatio, backgroundHeightRatio);
@@ -13,12 +14,22 @@ const menuState = {
 
         console.log(game.world.width, game.world.height);
 
-        gameInfo = game.add.bitmapText(30, 50, 'carrier_command', 'INSTRUCTION', titleFontSize);
+        instructionTitle = game.add.bitmapText(30, 40, 'carrier_command', 'INSTRUCTION', titleFontSize);
+        instructionText = game.add.bitmapText(30, 80, 'carrier_command',
+                                                'The main purpose of the game is to collect tools.\n\n' +
+                                                'They are randomly showing up - to reach them ' +
+                                                'you need to jump.\n\nYou also need to jump to avoid' +
+                                                ' a series of the obstacles.\n\nIf you will hit the' +
+                                                ' obstacle you will loose a life.\n\nYou have a total of' +
+                                                ' 10 lives in the game.\n\nAfter loosing last life ' +
+                                                'the game will end.\n\nthere is a special "golden" tool' +
+                                                ' which gives you an extra points.\n\nLook for it! Good luck!',
+                                                textFontSize);
+        gameControlsTitle = game.add.bitmapText(30, 360, 'carrier_command', 'GAME CONTROLS', titleFontSize);
+        gameControlsText = game.add.bitmapText(30, 400, 'carrier_command',
+                                                '[SPACEBAR] - JUMP (collect tools/avoid obstacles)', textFontSize)
 
-        gameInfoWd = game.add.bitmapText(30, 350, 'carrier_command', game.world.width, 10);
-        gameInfoHg = game.add.bitmapText(30, 370, 'carrier_command', game.world.height, 10);
-
-        const buttonStart = game.add.button(30, 100, 'buttonStart',
+        const buttonStart = game.add.button(30, 450, 'buttonStart',
                           menuState.start, menuState, 2, 1, 0); // start button
     },
 
